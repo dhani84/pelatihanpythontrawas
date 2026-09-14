92f07218a7d40268686b0dd10deb28a4002a9b5e
dhani84 <115673275+dhani84@users.noreply.github.com>
2026-09-14 09:19:29 +0700

    Create fungsiprint2.py

---
+++ fungsiprint2.py
@@ -0,0 +1,11 @@
+print("=== PORTAL PENDAFTARAN KKA ===")
+
+nama_pendaftar = input("Ketik nama lengkap: ")
+asal_sekolah = input("Ketik asal sekolah: ")
+minat_bakat = input("Apa keahlian Anda: ")
+
+print("\n--- DATA DIVERIFIKASI ---")
+print("Registrasi divalidasi atas nama:")
+print(nama_pendaftar)
+print("Berbasis di lembaga:")
+print(asal_sekolah)
