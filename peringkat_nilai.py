e2f0710a6cb2d53d062f44dc8a68a1982049dc6d
dhani84 <115673275+dhani84@users.noreply.github.com>
2026-09-14 09:24:45 +0700

    Create peringkat_nilai.py

---
+++ peringkat_nilai.py
@@ -0,0 +1,10 @@
+skor = int(input("Masukkan total nilai Anda (1-100): "))
+
+if skor >= 90:
+    print("Predikat: Amat Baik (A)")
+elif skor >= 80:
+    print("Predikat: Baik (B)")
+elif skor >= 70:
+    print("Predikat: Cukup (C)")
+else:
+    print("Predikat: Kurang (D)")
