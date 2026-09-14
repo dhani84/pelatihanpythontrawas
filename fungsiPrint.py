146a88dd96943c351faa147e1d8955051ea976e5
dhani84 <115673275+dhani84@users.noreply.github.com>
2026-09-14 09:17:00 +0700

    Create fungsiPrint.py

---
+++ fungsiPrint.py
@@ -0,0 +1,9 @@
+# Proyeksi Teks Literal
+print("Sistem KKA diinisialisasi.")
+print('Menunggu instruksi pengguna...')
+
+# Proyeksi Nilai Numerik
+print(2026)
+
+# Proyeksi Hasil Komputasi Langsung
+print(50 + 25)
