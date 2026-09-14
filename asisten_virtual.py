c347b8cc8b111bfb2f361c6dfcb327019aeadaba
dhani84 <115673275+dhani84@users.noreply.github.com>
2026-09-14 09:20:18 +0700

    Create asisten_virtual.py

---
+++ asisten_virtual.py
@@ -0,0 +1,10 @@
+print("Menghubungkan ke Asisten Virtual AI...")
+
+pengguna_aktif = input("Inisialisasi sistem. Siapa nama panggilan Anda? ")
+waktu_akses = input("Sebutkan waktu akses saat ini (Pagi/Siang/Malam): ")
+
+# Komputasi penggabungan teks
+respons_sistem = "Selamat " + waktu_akses + " Kak " + pengguna_aktif + "!"
+
+print("\n[AI Assistant]: " + respons_sistem)
+print("[AI Assistant]: Ada yang bisa saya bantu hari ini?")
