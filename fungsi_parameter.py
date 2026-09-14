ebf45197717c5136fb72e3fceb3b8b4ad1042a88
dhani84 <115673275+dhani84@users.noreply.github.com>
2026-09-14 09:28:21 +0700

    Create fungsi_parameter.py

---
+++ fungsi_parameter.py
@@ -0,0 +1,7 @@
+# 'nama_siswa' adalah parameter yang siap menerima data
+def sapa_personal(nama_siswa):
+    print(f"Halo, {nama_siswa}! Selamat belajar Python.")
+
+# 'Andi' dan 'Siti' adalah argumen (data nyata yang disetor)
+sapa_personal("Andi")
+sapa_personal("Siti")
